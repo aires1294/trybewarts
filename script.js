0f01fc337a350d2a2d142ac983d08ef7308bfbaa
Renan Campos <r.campos_@outlook.com>
2022-07-13 17:36:55 -0300

    item 3 e 4 - Renan

---
+++ script.js
@@ -0,0 +1,12 @@
+const login = document.getElementById ('email');
+const senha = document.getElementById ('password');
+const buttonRegister = document.getElementById ('entrar');
+
+function alerta (){
+        if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+        alert ("Olá, Tryber!");
+    }else {
+         alert ("Email ou senha inválidos.");
+    }
+}
+buttonRegister.addEventListener ('click', alerta);
