505b26cd3c5dcc4430c3ba31f78431769a3d1de8
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-14 21:14:17 -0300

    finalizado obrigatorios

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const login = document.getElementById('email');
-const senha = document.getElementById('password');
-const buttonRegister = document.getElementById('entrar');
+const login = document.getElementById("email");
+const senha = document.getElementById("password");
+const buttonRegister = document.getElementById("entrar");
+const checkBtn = document.getElementById("agreement");
+const subBtn = document.getElementById("submit-btn");
 
 function alerta() {
-  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert('Olá, Tryber!');
+  if (login.value === "tryber@teste.com" && senha.value === "123456") {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 }
-buttonRegister.addEventListener('click', alerta);
+buttonRegister.addEventListener("click", alerta);
+
+checkBtn.addEventListener("click", () => {
+  if (checkBtn.checked === true) {
+    subBtn.disabled = false;
+  } else {
+    subBtn.disabled = true;
+  }
+});
